01e65b832206ed6c422b20a1c2b64a973e6f607a
dogdave <daveoliver.home@gmail.com>
2017-06-05 13:27:13 -0600

    Hello World test

--- dogbot_main.js
+++ dogbot_main.js
@@ -1,4 +1,8 @@
-exports.handler = function (event, context) {
-    context.succeed('Hello 1984!');
-    };
-    
+var botBuilder = require('claudia-bot-builder'),
+    excuse = require('huh');
+
+module.exports = botBuilder(function (request) {
+    return 'Thanks for sending ' + request.text +
+        '. Your message is very boring to us, so ' +
+        excuse.get();
+});
