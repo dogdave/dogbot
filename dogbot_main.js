5637c6e6b23882399b30dc4e40de4e181238a6e9
dogdave <daveoliver.home@gmail.com>
2017-06-06 12:19:36 -0600

    Started type parsing
    Signed-off-by: dogdave <daveoliver.home@gmail.com>

--- dogbot_main.js
+++ dogbot_main.js
@@ -26,11 +26,15 @@ DogBot.prototype.respond = function (message, request) {
     if (message.text.includes("-verbose")){
         response += 'Full Original Message : ' + JSON.stringify(request) + '\r\r';
     }
+
+    // determine request type
+    var request_type = classify_request(message);
+
     // determine message type
     switch(message.type) {
         case 'slack-slash-command':
             if (debug){response += 'Message Type: Slack Command' + '\r';}
-            response += SlackResponse(message, request, debug);
+            response += SlackResponse(message, request, debug, request_type);
 
             break;
         case 'facebook':
@@ -42,16 +46,43 @@ DogBot.prototype.respond = function (message, request) {
     return response;
 };
 
-function SlackResponse(message, request, debug){
+function classify_request(message){
+    // What does the user want?
+    var request_type = '';
+
+    // Reservation Related
+    if (message.text.includes('reservation')) {
+        // Status of Reservation
+        if (message.text.includes('status')) {
+            request_type = "reservation_status";
+        }
+        // New Reservation Request
+        else if (message.text.includes('new')) {
+            request_type = "reservation_new";
+        }
+        else {
+            request_type = "reservation_unknown";
+        }
+    }
+    // Funny Dog Pictures
+
+    return request_type;
+};
+
+function SlackResponse(message, request, debug, request_type){
     var SlackResponse = '';
+    var User = message.sender;
+    var RequestTime =  new Date();
 
     if (debug){
         SlackResponse += 'Slack Debug On' + '\r';
-        SlackResponse += 'User: ' + message.sender + '\r';
-        SlackResponse += 'Time: ' + new Date().toLocaleString(); + '\r\r\n';
+        SlackResponse += 'User: ' + User + '\r';
+        SlackResponse += 'Time: ' + RequestTime.toLocaleString() + '\r';
     }
 
     var parse_index =0;
+
+    // New Reservation Request?
     parse_index = message.text.indexOf("reservation");
 
     if (debug){
@@ -64,14 +95,16 @@ function SlackResponse(message, request, debug){
         if(debug) {
             SlackResponse += 'Reservation String: ' + message.text.substring(parse_index) + '\r';
             SlackResponse += 'Moment Parse: ' + moment(message.text.substring(parse_index)).toLocaleString() + '\r';
+            SlackResponse += 'Request Type: ' + request_type + '\r';
         }
         var StartDate = new Date();
         var EndDate = new Date();
     }
 
+    // Reservation Status Request?
 
     return SlackResponse;
-}
+};
 
 
 module.exports = DogBot;
