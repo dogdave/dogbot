2d7c04b5ffc6a35d64d4926b8af0216ef3fbfa6b
dogdave <daveoliver.home@gmail.com>
2017-06-06 08:44:06 -0600

    Beginning debug statements
    Signed-off-by: dogdave <daveoliver.home@gmail.com>

--- bot.js
+++ bot.js
@@ -12,12 +12,4 @@ module.exports = botBuilder(function (message, originalRequest) {
 
 
 
-/*
-module.exports = botBuilder(function (message) {
-    console.log('Msg Received : ', message.text);
-    console.log('Full Message : ', JSON.stringify(message.originalRequest));
-    return 'Thanks for sending ' + message.text +
-        '. Your message is very boring to us, so ' +
-        excuse.get();
-});*/
 
--- dogbot_main.js
+++ dogbot_main.js
@@ -2,16 +2,43 @@
 
  */
 var DogBot = function (){};
-
 DogBot.prototype.respond = function (message, request) {
-    var response = 'Error, null response';
-    //debug
-    if (message.text.length >0){
-        response = 'Received : ' + message.text + '\r' +
-                'Full Original Message : ' + JSON.stringify(request);
+    var response = '';
+    var debug = false;
+
+    if (message.text.length === 0) {
+        response = 'Error, null message';
     }
+    else
+    {
+        response += 'Dogbot reporting for duty!' + '\r';
+    }
+    // general debug
+    if (message.text.includes("-debug")){
+    debug = true;
+    response += 'Received ' + message.text + '\r';
+    }
+    // verbose debug
+    if (message.text.includes("-verbose")){
+        response += 'Full Original Message : ' + JSON.stringify(request) + '\r\r';
+    }
+    // determine message type
+    switch(message.type) {
+        case 'slack-slash-command':
+            if (debug){
+                response += 'Message Type: Slack Command' + '\r';
+            }
+            break;
+        case 'facebook':
+            break;
+        default:
+            response += 'Message type undefined';
+    }
+
     return response;
 };
+
+
 
 module.exports = DogBot;
 /*
@@ -21,3 +48,12 @@ module.exports = botBuilder(function (message) {
         excuse.get() + '\r\r' +
         "Full message Text : " + JSON.stringify(message.originalRequest);
 });*/
+
+//scratchpad
+/*
+var test = "hello -debug";
+
+if (test.includes("-debugs")){
+    console.log('pass');
+}*/
+
